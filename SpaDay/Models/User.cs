0c89fd5abc935110d7477ebd777faa9cd4d2b88d
Ryan Chaffee <rchaff7@yahoo.com>
2020-12-06 01:57:51 -0600

    user class model

---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; }
+        public string Email { get; set; }
+        private string Password { get; set; }
+
+
+    }
+}
